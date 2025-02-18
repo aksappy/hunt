6f268a9af38ff41ef2e138939935ec6c37d64929
aksappy <aksappy@gmail.com>
2025-02-17 22:28:53 -0500

    feat: cli parsing using argh

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Welcome to the Hunt CLI!");
+use argh::FromArgs;
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Hunt - Index and Searching
+struct Arguments {
+    #[argh(positional)]
+    expression: String,
+
+    #[argh(positional)]
+    directories: Vec<String>
 }
+
+fn main() {
+    let up: Arguments = argh::from_env();
+    println!("{:?}", up);
+}
+
